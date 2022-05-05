5f62f417a5adfd32c7306c61903d9a3cc0e4837d
Andrew Moore <andrewdmoore84@gmail.com>
2022-05-05 15:53:40 -0400

    Add functionality to add a note to store and toggle its importance

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { createStore } from 'redux';
 import noteReducer from './reducers/noteReducer';
 
-const store = createStore(noteReducer);
+const store = createStore(
+  noteReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 store.dispatch({
   type: 'NEW_NOTE',
@@ -21,18 +24,52 @@ store.dispatch({
   },
 });
 
+const generateId = () =>
+  Number((Math.random() * 1000000).toFixed(0));
+
 const App = () => {
+  const addNote = (event) => {
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = '';
+
+    store.dispatch({
+      type: 'NEW_NOTE',
+      data: {
+        content,
+        important: false,
+        id: generateId(),
+      },
+    });
+  };
+
+  const toggleImportance = (id) => {
+    console.log('toggle!');
+    store.dispatch({
+      type: 'TOGGLE_IMPORTANCE',
+      data: { id },
+    });
+  };
+
   return (
     <div>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
       <ul>
+        {console.log('state: ', store.getState())}
         {store.getState().map(note =>
-          <li key={note.id}>
+          <li
+            key={note.id}
+            onClick={() => toggleImportance(note.id)}
+          >
             {note.content} <strong>{note.important ? 'important' : ''}</strong>
           </li>
         )}
       </ul>
     </div>
   );
-}
+};
 
 export default App;
--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,20 +1,21 @@
 const noteReducer = (state = [], action) => {
   switch (action.type) {
-    case 'NEW_NOTE':
+    case 'NEW_NOTE': {
       return [...state, action.data];
+    }
     case 'TOGGLE_IMPORTANCE': {
-        const id = action.data.id;
-        const noteToChange = state.find(n => n.id === id);
+      const id = action.data.id;
+      const noteToChange = state.find(n => n.id === id);
 
-        const changedNote = {
-          ...noteToChange,
-          important: !noteToChange.important,
-        };
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important,
+      };
 
-        return state.map(note =>
-          note.id !== id ? note : changedNote
-        );
-      }
+      return state.map(note =>
+        note.id !== id ? note : changedNote
+      );
+    }
     default:
       return state;
   }
